803b86b8b35173f0a6608453b94cbf0f4e66b08b
biazinha32 <106695231+biazinha32@users.noreply.github.com>
2024-09-04 15:52:30 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -56,7 +56,7 @@ const loop = () => {
       mario.classList.remove('.jump')
       mario.style.bottom = `${marioPosition}px`
 
-      mario.src = './src/img/game-over.png'
+      mario.src = 'src/img/XLIH.gif'
       mario.style.width = '80px'
       mario.style.marginLeft = '50px'
       
